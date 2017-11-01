a5245d10f7c6cd869d4ec8fa3072adb88618654e
MrArnon <32636989+MrArnon@users.noreply.github.com>
2017-11-01 20:50:42 +0300

    Add files via upload

---
+++ KMZI_5/KMZI_5/Prime_numbers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMZI_5
+{
+    class Prime_numbers
+    {
+        public bool IsPrime(int x)
+        {
+            if (x < 1) return false;
+            for (int i = 2; i * i <= x; i++)
+                if ((x % i) == 0) return false;
+            return true;
+        }
+        public int durak()
+        {
+            bool flag = false;
+            int R = 0;
+            // Console.WriteLine("Введите число");
+            string str;
+            str = Console.ReadLine();
+            try
+            {
+                R = Convert.ToInt32(str);
+                flag = true;
+            }
+            catch (Exception) { flag = false; Console.WriteLine("Неверный ввод"); }
+            if (flag) { return R; }
+            else { durak(); return 0; }
+        }
+        public void Eratosfen(int n)
+        {
+            //int n = int.Parse(Console.ReadLine());
+            bool[] A = new bool[n];
+            // Инициализация и вывод массива
+            for (int i = 2; i < n; i++)
+            {
+                A[i] = true;
+            //    Console.Write("{0} ", i);
+            }
+            
+            // Обработка
+            for (int i = 2; i < Math.Sqrt(n) + 1; ++i)
+            {
+                if (A[i])
+                {
+                    for (int j = i * i; j < n; j += i)
+                    {
+                        A[j] = false;
+                    }
+                }
+            }
+            Console.WriteLine();
+            // Повторный вывод
+            for (int i = 2; i < n; i++)
+            {
+                if (A[i])
+                    Console.Write("{0} ", i);
+            }
+        }
+        }
+}
---
+++ KMZI_5/KMZI_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMZI_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prime_numbers prime = new Prime_numbers();
+            int mode = -1;
+            Console.WriteLine("Для генерации простых чисел введите 0");
+            Console.WriteLine("Для проверки числа на простоту введите 1");
+            mode = prime.durak();
+            switch (mode)
+            {
+                case 0:
+                    Console.WriteLine("Введите до какого числа искать простые числа");
+                    int n = prime.durak();
+                        prime.Eratosfen(n);
+                        break;
+                case 1:
+                    Console.WriteLine("Введите число для проверки");
+                    int x = prime.durak();
+                    if (prime.IsPrime(x))
+                        Console.WriteLine("Число " + x + " простое");
+                    else Console.WriteLine("Число " + x + " не является простым");
+                    break;
+
+
+
+                default: Console.WriteLine("Неверный ввод"); break;
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
