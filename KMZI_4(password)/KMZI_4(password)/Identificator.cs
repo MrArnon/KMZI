97d693ffbeb2535ef62ff7bd63684383378ff9ca
MrArnon <32636989+MrArnon@users.noreply.github.com>
2017-11-01 19:58:25 +0300

    new mas S, without sqrt

--- KMZI_4(password)/KMZI_4(password)/Identificator.cs
+++ KMZI_4(password)/KMZI_4(password)/Identificator.cs
@@ -11,19 +11,21 @@ namespace KMZI_4_password_
     {
         public int X { get; set; }
         private int A = 5;
-        private int B = 7;
+        private int B = 13;
         public int N;    //19823=43 *461 //43*3=129
         public int[] V { get; set; }
         private int[] S { get; set; }
         private int[] V_obr { get; set; }
         public int[] Bool { get; set; } //private
         public int Y { get; set; } //private
+        public bool Flag { get; set; }
         public Identificator(int count)
         {
             V = new int[count];
             S = new int[count];
             V_obr = new int[count];
             Bool = new int[count];
+            //Flag = true;
             metod_N();
 
         }
@@ -31,34 +33,36 @@ namespace KMZI_4_password_
         {
             FileStream file_output = new FileStream("output.txt", FileMode.Create); //создаем файловый поток
             StreamWriter writer = new StreamWriter(file_output); //создаем «потоковый писатель» и связываем его с файловым потоком 
-            for (int i = 0; i < count; i++)
-        { 
-                writer.Write(S[i]);
-        }
+                                                                 //for (int i = 0; i < count; i++)
+                                                                 //{
+                                                                 // writer.Write(S[i]);
+                                                                 // }
+            writer.Write(Y);
             writer.WriteLine();
             writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется 
 
         }
-        public void ending(int new_Y,int count)
+        public void ending(int new_Y, int count)
         {
-            int temp = new_Y*new_Y;
+            int temp = new_Y * new_Y;
             for (int i = 0; i < count; i++)
             {
                 temp *= ((int)Math.Pow(S[i], Bool[i]));
             }
             temp = temp % N;
-            if(temp==X)
+            if (temp == X)
             {
                 Console.WriteLine("Проверка пройдена");
+                Flag = true;
             }
-            else Console.WriteLine("Проверка не пройдена");
+            else { Console.WriteLine("Проверка не пройдена"); Flag = false; }
         }
-        public void secret_Y(int R,int count)
+        public void secret_Y(int R, int count)
         {
             Y = R;
-            for (int i=0;i<count;i++)
+            for (int i = 0; i < count; i++)
             {
-                Y *= ( (int)Math.Pow(S[i] , Bool[i]));
+                Y *= ((int)Math.Pow(S[i], Bool[i]));
             }
             Y = Y % N;
         }
@@ -75,7 +79,7 @@ namespace KMZI_4_password_
             for (int i = 0; i < count; i++)
             {
                 V_obr[i] = 1;
-                while(V[i]*V_obr[i]% N !=1)
+                while (V[i] * V_obr[i] % N != 1)
                 {
                     V_obr[i]++;
                 }
@@ -84,38 +88,41 @@ namespace KMZI_4_password_
         public void Generate_S(int count)
         {
             value_VObr(count);
+
             for (int i = 0; i < count; i++)
-            {
+
                 S[i] = (int)Math.Sqrt(V_obr[i]) % N;
-            }
+
+
         }
         public void generate_V(int count)
         {
             for (int i = 0; i < count; i++)
             {
-                Random rnd = new Random(DateTime.Now.Millisecond);
-                int temp = rnd.Next(1, N);
-                V[i] = temp;
+                Random rnd = new Random();//(DateTime.Now.Millisecond);
+                //int temp = rnd.Next(1, N);
+                V[i] = rnd.Next(2, N + 1);
                 bool flag = true;
                 while (!control_V(V[i]) && flag)
                 {
-                    temp = rnd.Next(1, N);
-                    V[i] = temp;
+                    //rnd.Next(1, N);
+                    V[i] = rnd.Next(2, N + 1);
+                    //V[i] = temp;
                     try
                     {
                         flag = !(V[i] == V[i - 1]);
                     }
-                 catch(System.IndexOutOfRangeException) { flag = false; }
-                 }
+                    catch (System.IndexOutOfRangeException) { flag = false; }
+                }
 
             }
         }
         private bool control_V(int V)
         {
-            bool b=false;
-            
-            {if (V < N)
+            bool b = false;
 
+            {
+                if (V < N)
                 {
                     if (V % A != 0 && V % B != 0) b = true;
                     else b = false;
@@ -128,7 +135,7 @@ namespace KMZI_4_password_
         public void Generate_bool(int count)
         {
             Console.WriteLine("Последовательность для проверки");
-            for (int i=0;i<count;i++ )
+            for (int i = 0; i < count; i++)
             {
                 random_bits(i);
             }
@@ -136,46 +143,76 @@ namespace KMZI_4_password_
         }
         private void random_bits(int i)
         {
-             
-            Random rnd = new Random(DateTime.Now.Millisecond);
-            int temp = rnd.Next();
+
+            Random rnd = new Random();//(DateTime.Now.Millisecond);
+            //int temp = rnd.Next();
             bool b;
-            if (temp % 2 == 0) b = true;
-            else b= false;
+            if (rnd.Next(0, 1000) % 2 == 0) b = true;
+            else b = false;
             if (b)
             {
-                Console.Write('1'+" ");
+                Console.Write('1' + " ");
                 Bool[i] = 1;
-              
-                
+
+
             }
             else
             {
-                Console.Write('0'+ " ");
+                Console.Write('0' + " ");
                 Bool[i] = 0;
-             
+
             }
             //Console.WriteLine("");
         }
-       public int durak()
-        
-            {
+        public int durak()
+        {
             bool flag = false;
             int R = 0;
-           // Console.WriteLine("Введите число");
-                string str;
-                str = Console.ReadLine();
-                try
-                {
-                    R = Convert.ToInt32(str);
-                    flag = true;
-                }
-                catch (Exception) { flag = false; Console.WriteLine("Неверный ввод"); }
+            // Console.WriteLine("Введите число");
+            string str;
+            str = Console.ReadLine();
+            try
+            {
+                R = Convert.ToInt32(str);
+                flag = true;
+            }
+            catch (Exception) { flag = false; Console.WriteLine("Неверный ввод"); }
             if (flag) { return R; }
-            else { durak();return 0; }
+            else { durak(); return 0; }
         }
-         
+        public void V_b_Click(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int V_temp = V[i];
+
+
+                if (V_temp % A == 0 || V_temp % B == 0 || V_temp > N) { Console.WriteLine("Ошибка в массиве V"); }
+                else
+                {
+                    int VI = 0;
+                    for (int j = 1; j < N * 2; j++)
+                    {
+                        if ((V_temp * j) % N == 1)
+                        { VI = j; break; }
+
+
+                    }
+
+                    V_obr[i] = VI;
+
+                    int k = VI;
+                    int S_temp = 0;
+                    int m = 1;
+                    while ((m * m) % N != k) m++;
+                    S_temp = m;
+                    S[i] = S_temp;
+
+                }
+            }
+
         }
     }
+}
 
 
--- KMZI_4(password)/KMZI_4(password)/Program.cs
+++ KMZI_4(password)/KMZI_4(password)/Program.cs
@@ -12,31 +12,50 @@ namespace KMZI_4_password_
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int temp = rnd.Next(4,4);
+            int temp = rnd.Next(2, 4);
             Identificator game = new Identificator(temp);
-            game.generate_V(temp);
-            /*game.V[0] = 4;
-            game.V[1] = 11;
-            game.V[2] = 16;
-            game.V[3] = 29;
-    */        
-            Console.WriteLine("Введите число R меньшее " + game.N);
-            int R = game.durak();
-            while(R>=game.N)
+            game.Flag = true;
+            int i = 0;
+            int R_old = 0;
+            while (game.Flag && i < 3)
             {
+
+
+
+                temp = rnd.Next(4, 4);
+                game = new Identificator(temp);
+               // game.generate_V(temp);
+                      game.V[0] = 4;
+                      game.V[1] = 9;
+                      game.V[2] = 16;
+                      game.V[3] = 29;
+                game.V_b_Click(temp);
                 Console.WriteLine("Введите число R меньшее " + game.N);
-                R = game.durak();
+                int R = game.durak();
+                while (R >= game.N)
+                {
+                    Console.WriteLine("Введите число R меньшее " + game.N);
+                    R = game.durak();
+                }
+                if (R_old != R)
+                {
+                    game.metod_X(R);
+                   // game.Generate_S(temp);
+
+                    game.Generate_bool(temp);
+                    game.secret_Y(R, temp);
+                    game.writer(temp);
+                    Console.WriteLine("Введите число Y");
+                    int new_Y = game.durak();
+
+                    game.ending(new_Y, temp);
+                    R_old = R;
+                   // game.ending(game.Y, temp);
+
+                    i++;
+                }
+                else { R_old = R; game.Flag = false; i++; Console.WriteLine("Проверка не пройдена"); }
             }
-            game.metod_X(R);
-            game.Generate_S(temp);
-            game.writer(temp);
-            game.Generate_bool(temp);
-            game.secret_Y(R, temp);
-            Console.WriteLine("Введите число Y");
-            int new_Y = game.durak();
-           
-            game.ending(new_Y,temp);
-            //game.ending(game.Y, temp);
             Console.ReadLine();
 
         }
