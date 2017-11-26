271e1291fd594ba60bafb405160ddced6689db10
MrArnon <32636989+MrArnon@users.noreply.github.com>
2017-11-26 19:00:46 +0300

    with OOP

--- KMZI_2/KMZI_2/Form1.cs
+++ KMZI_2/KMZI_2/Form1.cs
@@ -16,55 +16,15 @@ namespace KMZI_2
         {
             InitializeComponent();
         }
-        const int kol_liter = 64;//32+32 буквы русского алфавита большие и маленькие (без букв Ё ё )
         private void Еncryption_button_Click(object sender, EventArgs e)
         {
-            List<Liter> spisok = new List<Liter>();
-            int j = 1040;//большая буква А=192 русская в Unicode А=1040 - 1103
-            for (int i = 0; i < kol_liter; i++)
-            {
-                Liter temp = new Liter(j);
-                spisok.Add(temp);
-                j++;
-            }
-            // 32 - ' ' 33 - '!'    44 - ','    46 - '.'    63 -'?'    13 - 'Enter'
-            Liter temp1 = new Liter(32);
-            spisok.Add(temp1);
-            Liter temp2 = new Liter(33);
-            spisok.Add(temp2);
-            Liter temp3 = new Liter(44);
-            spisok.Add(temp3);
-            Liter temp4 = new Liter(46);
-            spisok.Add(temp4);
-            Liter temp5 = new Liter(63);
-            spisok.Add(temp5);
-            Liter temp6 = new Liter(13);
-            spisok.Add(temp6);
-            string info = Insert_text_box.Text;
-            string new_info=string.Empty;
-            for (int i=0;i<info.Length;i++)
-            {
-                char bukva = info[i];
-                int p = 0;
-                bool b = false;
-                while (p<spisok.Count && !b)
-                {
-                    Liter temp = spisok[p];
-                    b = bukva == temp.lit;
-                    if (b) {
-                        int conv = temp.Pop(temp.count);
-                        new_info += conv.ToString();
-                        temp.count++;
-                        temp.Control(); }
-                    spisok[p] = temp;
-                    p++;
-                }
-            }
-            spisok.Clear();
-            Shifr_text.Text = new_info;
+			Liter liter = new Liter();
+			string info = Insert_text_box.Text;
+
+			Shifr_text.Text = liter.Encript(info);
             FileStream file_output = new FileStream("output.txt", FileMode.Create); //создаем файловый поток
             StreamWriter writer = new StreamWriter(file_output); //создаем «потоковый писатель» и связываем его с файловым потоком 
-            writer.Write(new_info); //записываем в файл
+            writer.Write(Shifr_text.Text); //записываем в файл
             writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется 
 
         }
@@ -76,74 +36,13 @@ namespace KMZI_2
 
         private void Transcript_button_Click(object sender, EventArgs e)
         {
-            const int n_mas = 4;
-            List<Liter> spisok = new List<Liter>();
-            int j = 1040;//большая буква А=192 русская в Unicode А=1040 - 1103
-            for (int i = 0; i < kol_liter; i++)
-            {
-                Liter temp = new Liter(j);
-                spisok.Add(temp);
-                j++;
-            }
-            Liter temp1 = new Liter(32);
-            spisok.Add(temp1);
-            Liter temp2 = new Liter(33);
-            spisok.Add(temp2);
-            Liter temp3 = new Liter(44);
-            spisok.Add(temp3);
-            Liter temp4 = new Liter(46);
-            spisok.Add(temp4);
-            Liter temp5 = new Liter(63);
-            spisok.Add(temp5);
-            Liter temp6 = new Liter(13);
-            spisok.Add(temp6);
-            string info = Insert_text_box.Text;
-            string new_info = string.Empty;
-            for (int i = 0; i < info.Length; i++)
-            {
-                string bukva = string.Empty;
-                for (int k = 0; k < n_mas; k++)
-                {
-                    
-                    try
-                    {
-                        bukva += info[i];
-                    }
-                    catch(Exception) { MessageBox.Show("Неверно введенные данные"); }
-                    i++;
-                }
-                i--;//костыль
-                int p = 0;
-                bool b = false;
-                while (p < spisok.Count && !b)
-                {
-                    Liter temp = spisok[p];
-                    string str = temp.Pop(temp.count).ToString();
-                    if (str == "9999" || str == "9898" || str == "9779" || str == "9000")
-                    {
-                        new_info += "\r\n";
-                        temp.count++;
-                        temp.Control();
-                    }
-                    else
-                    {
-                        b = bukva == str;
-                        if (b)
-                        {
-                            new_info += temp.lit;
-                            temp.count++;
-                            temp.Control();
-                        }
-                    }
-                        spisok[p] = temp;
-                    p++;
-                }
-            }
-            spisok.Clear();
-            Shifr_text.Text = new_info;
+			Liter liter = new Liter();
+			string info = Insert_text_box.Text;
+        
+            Shifr_text.Text = liter.Transcript(info);
             FileStream file_output = new FileStream("output.txt", FileMode.Create); //создаем файловый поток
             StreamWriter writer = new StreamWriter(file_output); //создаем «потоковый писатель» и связываем его с файловым потоком 
-            writer.Write(new_info); //записываем в файл
+            writer.Write(Shifr_text.Text); //записываем в файл
             writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется 
 
         }
